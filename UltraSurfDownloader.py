9101bff757dcd77c0be62b67baf0721800c113b8
Kaiser Paul <107715370+callmekaii@users.noreply.github.com>
2025-05-09 18:02:17 +0800

    Code Refactoring

--- UltraSurfDownloader.py
+++ UltraSurfDownloader.py
@@ -25,8 +25,12 @@ filename = "mjnbclmflcpookeapghfhapeffmpodij.zip"
 if os.path.isfile("mjnbclmflcpookeapghfhapeffmpodij/app.js"):
     pass
 else:
-    wget.download('https://mrpa-crx-1308499021.cos.na-siliconvalley.myqcloud.com/recommend/vpn/mjnbclmflcpookeapghfhapeffmpodij/mjnbclmflcpookeapghfhapeffmpodij.crx?q-sign-algorithm=sha1&q-ak=AKIDnhS6YnoY1U5rdKmxtcx87tRWaVGn580c&q-sign-time=1725206395%3B1882886455&q-key-time=1725206395%3B1882886455&q-header-list=host&q-url-param-list=&q-signature=f679842c57f5659a9bfa147dbe9bc04db2c7ff9c', filename)
-    #This kinda extracts the good stuff then finally deletes it
-    ZipFile(filename).extractall("mjnbclmflcpookeapghfhapeffmpodij")
-    os.remove(filename)
-input("\nPress Enter to exit...")
+    try:
+        wget.download('https://mrpa-crx-1308499021.cos.na-siliconvalley.myqcloud.com/recommend/vpn/mjnbclmflcpookeapghfhapeffmpodij/mjnbclmflcpookeapghfhapeffmpodij.crx?q-sign-algorithm=sha1&q-ak=AKIDnhS6YnoY1U5rdKmxtcx87tRWaVGn580c&q-sign-time=1725206395%3B1882886455&q-key-time=1725206395%3B1882886455&q-header-list=host&q-url-param-list=&q-signature=f679842c57f5659a9bfa147dbe9bc04db2c7ff9c', filename)
+        #This kinda extracts the good stuff then finally deletes it
+        ZipFile(filename).extractall("mjnbclmflcpookeapghfhapeffmpodij")
+        os.remove(filename)
+        input("\nPress Enter to exit...")
+    except:
+        input("No internet connection")
+
